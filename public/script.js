e6e9e64cb48a6c3fced20b23f6ae98e68d523c1c
mb-1208 <mbntngfadlillahh1212@gmail.com>
2022-02-18 20:43:03 +0700

    Push Project to Github

---
+++ public/script.js
@@ -0,0 +1,122 @@
+var textWrapper = document.querySelector(".intro-title");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+    /([^\x00-\x80]|\w)/g,
+    "<span class='letter'>$&</span>"
+);
+
+anime
+    .timeline({ loop: false })
+    .add({
+        targets: ".intro-title .letter",
+        translateX: [140, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1400,
+        delay: function (el, i) {
+            return 500 + 50 * i;
+        }
+    })
+    .add({
+        targets: ".intro-title .letter",
+        translateX: [0, -140],
+        opacity: [1, 0],
+        easing: "easeInExpo",
+        duration: 1200,
+        delay: function (el, i) {
+            return 700 + 50 * i;
+        }
+    });
+
+TweenMax.to(".loader", 2.2, {
+    delay: 5,
+    top: "-110%",
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".logo", 2, {
+    delay: 6,
+    y: 10,
+    opacity: 0,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".right-bottom-text", 2, {
+    delay: 6.7,
+    y: 10,
+    opacity: 0,
+    ease: Expo.easeInOut
+});
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else if (window.scrollY > 600) {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+var items = document.querySelectorAll(".timeline li");
+
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+        if (isElementInViewport(items[i])) {
+            if (!items[i].classList.contains("in-view")) {
+                items[i].classList.add("in-view");
+            }
+        } else if (items[i].classList.contains("in-view")) {
+            items[i].classList.remove("in-view");
+        }
+    }
+}
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
